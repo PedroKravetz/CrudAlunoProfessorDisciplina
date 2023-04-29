5fc0be5def76e53653245fbef402adbfa4c4e26f
PedroKravetz <p.v.kravetz@gmail.com>
2023-04-29 20:14:23 -0300

    Adição de Dialogs para Aluno e Disciplina

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,6 @@
 package main;
 
-import crudprofessor.ProfessorInternalFrame;
+import crudProfessor.ProfessorInternalFrame;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
